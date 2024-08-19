a945923b65fc7cda37d6ec50efbc2df47194f30d
TNejk <100270199+TNejk@users.noreply.github.com>
2024-08-19 15:55:26 +0200

    1.3

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import oracledb
 username = 'ADMIN'
 password = 'S0s-it-0racle'
 tls_string = '(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.eu-frankfurt-1.oraclecloud.com))(connect_data=(service_name=g174432cd8ec6d2_basedatabase_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'
-cursor = None
 
 def connect_to_database():
     print("Pripájam sa do databázy...")
@@ -11,9 +10,22 @@ def connect_to_database():
         connection = oracledb.connect(user=username, password=password, dsn=tls_string)
         print('Pripojil som sa do databázy!')
         cursor = connection.cursor()
-        return connection
+        return connection, cursor
     except oracledb.Error as error:
         print('Chyba v pripájaní do databázy:', error)
         return None
 
-connect_to_database()
+def try_commit():
+    try:
+        connection.commit()
+    except oracledb.Error as error:
+        print(error)
+        connection.rollback()
+
+connection, cursor = connect_to_database()
+
+request = cursor.execute("INSERT INTO Test (test_id, test_string, test_float) VALUES (0, 'Ahoj', 2.54)")
+try_commit()
+request = cursor.execute("SELECT * FROM Test")
+result = request.fetchall()
+print(result)
